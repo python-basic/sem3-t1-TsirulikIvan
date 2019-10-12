a579f3209f7977a9d1e2ac9e90943000d6d1d795
Tsirulik Ivan <31895996+TsirulikIvan@users.noreply.github.com>
2019-10-12 18:20:50 +0300

    Update geron.py

--- geron.py
+++ geron.py
@@ -1,9 +1,9 @@
 import math
 
 def geron():
-    a = input('Enter size of first side:\n')
-    b = input('Enter size of second side:\n')
-    c = input('Enter size of third side:\n')
+    a = float(input('Enter size of first side:\n'))
+    b = float(input('Enter size of second side:\n'))
+    c = float(input('Enter size of third side:\n'))
     p = (a + b + c) / 2
     return math.sqrt(p * (p - a) * (p - b) * (p - c))
 
